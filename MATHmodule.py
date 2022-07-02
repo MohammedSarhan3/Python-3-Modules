8f88559afedea8b00c38884d3a6854e3959d6df1
Mohammad Wael Sarhan <saraheeeen@gmail.com>
2022-07-02 18:02:12 +0200

    MATHmodule

---
+++ MATHmodule.py
@@ -0,0 +1,9 @@
+import math
+#print(dir(math))
+print(math.pi)
+print(math.log(100))
+print(math.exp(10))
+print(math.pow(10,2))
+print(math.sqrt(10))
+print(math.floor(10.2))
+print(math.ceil(10.2))
