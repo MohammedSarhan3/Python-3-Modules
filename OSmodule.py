10b5cd43e87df9a49d8f3cac7227fe19f1ac1469
Mohammad Wael Sarhan <saraheeeen@gmail.com>
2022-07-02 18:03:08 +0200

    OSmodule

---
+++ OSmodule.py
@@ -0,0 +1,14 @@
+import os
+#print(dir(os))--> to print command of modules
+#os.mkdir('abo sarhan') --> to make one file
+#os.makedirs('abo sarhan2/body/elharoon/ali/amr') ---> to make files inside file
+#os.chdir('abo sarhan2/body') --> to change dicrection
+#print(os.getcwd()) --> to show path
+#print(os.listdir('.'))    --> to show folders in path
+'''for dirpath , dirname , filenames in os.walk('.'):
+    print(f'current path : {dirpath}')
+    print(f'dirs : {dirname}')
+    print('------------')'''
+#print(os.path.isdir('abo sarhan'))--->is folder
+#print(os.path.isfile('abo sarhan')) ---> is file
+    
